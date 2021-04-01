b397888d9d1890bc5bd362ee0db8a2804f35cfa8
srushti8 <76839929+srushti8@users.noreply.github.com>
2021-04-01 19:54:56 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ const Body = Matter.Body;
 
 var ground, hammer;
 var rubber, stone1, stone2, stone3;
+var iron;
 
 function preload()
 {
@@ -22,8 +23,9 @@ function setup() {
 	ground = new Ground(400,700,800,10);
 	hammer = new Hammer(400,400,90,20);
 	rubber = new Rubber(10,20,200,200);
-	stone1 = new Stones(10,20,200,400);
-	stone2 = new Stones(50,50,200,500)
+	stone1 = new Stones(100,200,20);
+	stone2 = new Stones(250,350,20);
+	iron = new Iron(20,40,100,300)
 
 }
 
@@ -37,6 +39,7 @@ function draw() {
 	rubber.display();
 	stone1.display();
 	stone2.display();
+	iron.display();
 
   drawSprites();
  
